2e7bb36d5a6669ef7ce6d54c6da0f7db91256084
Babban33 <chaitanyawankhede705@gmail.com>
2024-09-26 19:22:47 +0530

    initizled all routes

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File
 from fastapi.middleware.cors import CORSMiddleware
-import os
-import tempfile
 from ultralytics import YOLO
-import base64
-from fastapi.responses import JSONResponse
-import json
+from osmf import router as osmf_router
+from calculus import router as calculus_router
+from gingivitis import router as gingi_router
+from phenotype import router as pheno_router
 
 app = FastAPI()
 
@@ -22,29 +21,7 @@ osmf = YOLO('./best.pt', task="classify")
 async def main():
     return {"message": "Hello World"}
 
-@app.post('/osmf')
-async def osmf_detection(file: UploadFile = None):
-    if file is None:
-        raise HTTPException(status_code=400, detail="No file provided")
-    try:
-        if file:
-            with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as temp_file:
-                temp_file.write(file.file.read())
-            
-            predict = osmf(temp_file.name)
-            js = predict[0].tojson()
-            predict_dict = json.loads(js)
-            name = predict_dict[0]["name"]
-            confidence = predict_dict[0]["confidence"]
-            print(name)
-            predict[0].save('./osmf-class.jpg')
-            os.remove(temp_file.name)
-            with open('./osmf-class.jpg', "rb") as image_file:
-                encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
-            result = {'status': 'success', 'generatedImage': encoded_image, 'class': name, 'conf': confidence}
-            os.remove('./osmf-class.jpg')
-            return JSONResponse(content=result)
-        else:
-            return{'status': 'error with file'}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+app.include_router(osmf_router)
+app.include_router(calculus_router)
+app.include_router(gingi_router)
+app.include_router(pheno_router)
